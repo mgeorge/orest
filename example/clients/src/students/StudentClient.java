1aaa1b09e1e9f7d0f4d9096fd18905db2cc5545b
Mark <mgeorge@infoscience.otago.ac.nz>
2011-03-29 19:31:53 +1300

    Improved StudentClient so it can be used as a functional test for testing all HTTP methods.

--- example/clients/src/students/StudentClient.java
+++ example/clients/src/students/StudentClient.java
@@ -1,9 +1,9 @@
 package students;
 
 import nz.ac.otago.orest.client.BadResponse;
-import students.domain.Student;
 import nz.ac.otago.orest.client.RestClient;
 import nz.ac.otago.orest.client.RestResponse;
+import students.domain.Student;
 
 /**
  *
@@ -15,28 +15,46 @@ public class StudentClient {
       RestClient c = new RestClient("http://localhost:8080/orest-example-services/rest/");
       c.addResourceType(Student.class, "student");
 
-      Student s = new Student(555, "Da Devil");
+      System.out.println("\n-- Testing post");
+      c.post("students", new Student(7744, "Fred"));
+      System.out.println(c.getCollection("students"));
+
+      System.out.println("\n-- Testing get with content-type");
+
+      RestResponse r = c.get("students/7744", "application/json");
+      System.out.println(r.getBody());
+      System.out.println(r.getStatus());
+      System.out.println(r.getMessage());
+      System.out.println(r.getContentType());
+
+      System.out.println("\n-- Testing get");
+
+      Student student = (Student) c.get("students/7744");
+      System.out.println(student);
+
+      System.out.println("\n-- Testing getCollection");
+
+      System.out.println(c.getCollection("students"));
+
+      System.out.println("\n-- Testing put");
+
+      c.put("students/7744", new Student(null, "Bub"));
+      student = (Student) c.get("students/7744");
+      System.out.println(student);
+
+      System.out.println("\n-- Testing delete");
+
+      c.delete("students/7744");
+
+      System.out.println(c.getCollection("students"));
+
+      System.out.println("\n-- Testing post to wrong URL");
 
       try {
-         c.post("students/1234", s);
+         c.post("students/1234", new Student(444, "Jim"));
       } catch (BadResponse ex) {
          System.out.println(ex.getMessage());
       }
-
-//      RestResponse r = c.get("students/1234", "application/json");
-//      System.out.println(r.getBody());
-//      System.out.println(r.getCode());
-//      System.out.println(r.getMessage());
-//      System.out.println(r.getContentType());
-//
-//      RestResource res = c.get("students/1234");
-//      System.out.println(res);
-//
-      System.out.println(c.getCollection("students"));
-//
-//      c.delete("students/1234");
-//
-//      System.out.println(c.getCollection("students"));
 
    }
 }
